79447171f7f4f6391619260e9d3f1b955f15254e
enzo25preisler <enzopreisler25@gmail.com>
2023-07-25 15:04:06 -0300

    first commit

---
+++ src/main/java/com/enzo25preisler/organizadorestudo/OrganizadorestudoApplication.java
@@ -0,0 +1,13 @@
+package com.enzo25preisler.organizadorestudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorestudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorestudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enzo25preisler/organizadorestudo/OrganizadorestudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzo25preisler.organizadorestudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadorestudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
